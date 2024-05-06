0ff79e062349ed6fae9bf73c5cd31de25d35e2f3
g;itchtonny <sirmwangitosh@gmail.com>
2024-05-07 00:39:43 +0300

    commit changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-/*function fetchDrinks(){
-    fetch("https://my-json-server.typicode.com/glitchtonny/fakeapi")
+
+
+      function fetchDrinks(){
+
+    fetch("https://api.myjson.online/v1/records/640ff52d-a7c1-41ff-bdb1-27130c6d858a")
     .then(res => res.json())
     .then(data => {
         console.log(data)
 
         
-        if (data.data) {
+        if (data.data.cocktail) {
           // Access the drinks property and iterate over its values
-          Object.values(data.data).forEach(drink => {
+          Object.values(data.data.cocktail).forEach(drink => {
               // Assuming addDrink is a function that adds the drink to the UI
               addDrink(drink);
           });
@@ -22,145 +25,10 @@
       
   });
 }
-fetchDrinks()*/
-
-/*function addDrink(drink){
-let div = document.getElementById('drink-collection')
-let card = document.createElement("div")
-div.appendChild(card)
-card.className= "card"
-
-let flipCardInner=document.createElement("div")
-card.appendChild(flipCardInner)
-flipCardInner.className = "flip-card-inner"
-
-let flipCardFront = document.createElement("div")
-flipCardInner.appendChild(flipCardFront)
-flipCardFront.className = "flip-card-front"
-
-let flipCardBack = document.createElement("div")
-flipCardInner.appendChild(flipCardBack)
-flipCardBack.className = "flip-card-back"
+fetchDrinks()
 
 
-let h4 = document.createElement("h4")
-  flipCardFront.appendChild(h4)
-  let name = drink.name
-  h4.innerText = name
-
-
-  //let image = document.createElement("img")
-  //flipCardFront.appendChild(image)
-  //image.className ="toy-avatar"
- // let imgURL = drink.image
-  //image.src = drink.image
-
-  
-
-  
-  let p = document.createElement("p")
-  p.innerText =`${drink.likes} likes` 
-  flipCardFront.append(p)
-
-  let button = document.createElement("button")
-  card.append(button)
-  button.className = "like-btn"
-  button.id = drink.id
-  button.innerText = "♥"
-  button.addEventListener('click',(e) => {
-  console.log("event", e)
-    e.preventDefault()
-    increment(e)
-}
-    )
-
-}
-
-
-function increment(e) {
-  
-  let count = parseInt(e.target.previousElementSibling.innerText) + 1;  
-  
-   
-  
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita/${e.target.id}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept" : "application/json"
-          },
-          
-          body: JSON.stringify({
-            likes: count,
-          })
-      })
-     .then((response) => response.json())
-     .then((json) => console.log(json));
-      
-}*/
- 
-
-  /*function addNewDrink() {
-    const getButton = document.querySelector(".add-drink");
-  
-    getButton.addEventListener("submit", (event) => {
-      event.preventDefault();
-      
-      const input = (document.querySelectorAll(".input-text"));
-  
-      const postRequest = {
-        method: "POST",
-        
-        body: JSON.stringify({
-          name: input[0].value,
-          image: input[1].value,
-          likes: 0,
-        }),
-      };
-  
-      fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita", postRequest)
-      .then((res) => res.json())
-      .then(res => console.log(res)) 
-      console.log(input[0].value);
-      console.log(input[1].value);
-    });
-  }
-  addNewDrink()*/
-
-
-
-  /*fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-
-    data.drinks.forEach(drink => {
-      addDrink(drink)
-    })
-  } )
-  .catch(error => console.error('Error:', error));
-
-
-  function addDrink(drink){
-    let div = document.getElementById('drink-collection')
-    let card = document.createElement("div")
-    div.appendChild(card)
-    card.className= "card"
-  }*/
-
-
-function fetchDrinks() {
-  fetch("http://localhost:3000/cocktail") 
-    .then((res) => res.json())
-    .then((res) => {
-      console.log("response2", res);
-      res.forEach((drink) => addDrink(drink));
-    });
-  }
-
-  fetchDrinks()
-
-  function addDrink(drink){
+function addDrink(drink){
     let div = document.getElementById('drink-collection')
     let card = document.createElement("div")
     div.appendChild(card)
@@ -211,8 +79,6 @@ function fetchDrinks() {
         )
     
     }
-    
-    
     function increment(e) {
       
       let count = parseInt(e.target.previousElementSibling.innerText) + 1;  
@@ -262,6 +128,12 @@ function fetchDrinks() {
         });
       }
       addNewDrink()
+
+
+
+
+
+
 
 
 
